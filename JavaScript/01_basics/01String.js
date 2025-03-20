05ef7cb08810455d7076d8109430aed34ac302c9
Rajan Sharma <140615149+Rajan-sharma-0@users.noreply.github.com>
2025-03-20 18:41:52 +0530

    Add files via upload

---
+++ JavaScript/01_basics/01String.js
@@ -0,0 +1,35 @@
+const name = "Rajan"
+const num = 50
+
+// console.log(`Hello ${name} and ${num}`)
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.substring(0, 3))
+// console.log(name[3])
+// console.log(name.length)
+// console.log(num.length)            // undefined because number has no length property
+
+
+
+const gamenaem = new String("   ClashofClans     ")  // This is antoher way to create string
+// console.log(gamenaem)
+// console.log(gamenaem[4]);
+
+
+const anothergamenaem = "ClashofClans"
+// console.log(anothergamenaem.slice(0, 5));  // It will print the string from 2 to 5
+// console.log(anothergamenaem.slice(-5));  // It will print the string from last 5 characters
+
+const game = "    ClashofClans    "
+// console.log(game);
+// console.log(game.trim());  // It will remove the extra spaces from the string and end and start
+
+
+
+
+
+
+
+
+
---
+++ JavaScript/01_basics/02Number_and math .js
@@ -0,0 +1,56 @@
+const score = 40
+// console.log(score);
+
+const number = new Number(40)
+// console.log(number);
+
+// console.log(number.toString().length);
+// console.log(number.toFixed(1));  // It will print the number with 2 decimal points
+
+const num = 50.434343
+
+// console.log(num.toPrecision(2)); // toPrecision() method returns a string, with a number written with a specified length
+
+const hundred = 1000000000
+// console.log(hundred.toLocaleString('en-IN'));  // toLocaleString() method returns a string with a language-sensitive representation of this number.
+
+
+
+// +++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++?//////
+
+
+// console.log(Math);
+
+
+// console.log(Math.abs(-4.7));              // It will print the absolute value of the number
+
+
+// console.log(Math.round(4.7));            // It will print the round off value of the number
+// console.log(Math.ceil(4.4));                //ceil() method rounds a number UPWARDS to the nearest integer, and returns the result.
+
+
+
+// console.log(Math.floor(4.9));           //floor() method rounds a number DOWNWARDS to the nearest integer, and returns the result
+
+
+
+// console.log(Math.sqrt(36));
+// console.log(Math.pow(2, 3));
+
+
+// console.log(Math.min(2, 3, 4, 5, 6, 7));
+// console.log(Math.max(2, 3, 4, 5, 6, 7));
+
+
+console.log(Math.random());                         
+console.log(Math.random() * 10);
+console.log(Math.floor(Math.random() * 10) + 1);     
+
+
+const min = 1
+const max = 6
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
