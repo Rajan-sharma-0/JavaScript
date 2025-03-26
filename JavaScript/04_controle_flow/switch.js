6d9d2a4a39087996ef79367a39c35a187077f806
Rajan-sharma-0 <nk800109@gmail.com>
2025-03-26 22:26:05 +0530

    controle_flow

---
+++ JavaScript/04_controle_flow/switch.js
@@ -0,0 +1,32 @@
+//                                                   Basic syntex
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 'apr';
+switch(month){
+    case "jan":
+        console.log("jan");
+        break;
+    case 'feb':
+        console.log("feb");
+        break;
+    case 'mar':
+        console.log("mar");
+        break;
+    case 'apr':
+        console.log("apr");
+        break;
+     
+    default:
+        console.log('nothing');
+        
+        break;
+        
+}
