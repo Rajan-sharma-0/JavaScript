d7722ed5bf7a1e17d580c63b6d49b2c21dde99f7
Rajan-sharma-0 <nk800109@gmail.com>
2025-05-08 05:57:43 +0530

    c

---
+++ JavaScript/10_Classes_and_oop/call.js
@@ -0,0 +1,20 @@
+
+function studentAddress  (address)  {
+    this.address = address
+    console.log('called  ');
+    
+}
+
+
+
+
+function studentData(name, age, roleNo, address){
+    this.name = name
+    this.age = age
+    this.roleNo = roleNo
+    studentAddress.call(this, address)
+    
+}
+
+const student = new studentData('rajan', 22, 1234, 'm-block')
+console.log(student);
