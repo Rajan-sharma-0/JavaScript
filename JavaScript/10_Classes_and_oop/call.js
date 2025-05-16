1978063aa7b7560c86b88c30ee44a2f2760f316e
Rajan-sharma-0 <nk800109@gmail.com>
2025-05-16 20:19:10 +0530

    add

--- JavaScript/10_Classes_and_oop/call.js
+++ JavaScript/10_Classes_and_oop/call.js
@@ -13,8 +13,15 @@ function studentData(name, age, roleNo, address){
     this.age = age
     this.roleNo = roleNo
     studentAddress.call(this, address)
-    
+      
 }
 
 const student = new studentData('rajan', 22, 1234, 'm-block')
-console.log(student);    
+console.log(student);  
+
+
+ prime = function(num){
+    if(num === 2)return true
+
+    
+}
