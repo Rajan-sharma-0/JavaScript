beaac370c745d4548656a7bd8891ab62e7364cca
Rajan-sharma-0 <nk800109@gmail.com>
2025-07-06 21:13:20 +0530

    loop

---
+++ JavaScript and DSA/Loops/01_.js
@@ -0,0 +1,34 @@
+let unit = Number(prompt("Enter the units: "));
+let amount = 0;
+// if(unit > 400){
+//     amount += (unit-400) * 13;
+//     unit = 400
+// }
+// if(unit > 200 && unit <= 400){
+//     amount += (unit-200) * 8;
+//     unit = 200
+// }
+// if(unit > 100 && unit <= 200){
+//     amount += (unit-100) * 6;
+//     unit = 100
+// }
+// amount += unit * 4
+
+
+///////////////////////// Using SWITCH CASE ///////////////////
+// switch (true) {
+//   case (unit > 400):
+//     amount += (unit - 400) * 13;
+//     unit = 400
+//   case (unit > 200 && unit <= 400):
+//     amount += (unit - 200) * 8;
+//     unit = 200;
+//   case (unit > 100 && unit <= 200):
+//     amount += (unit - 100) * 6;
+//     unit = 100;
+//   default:
+//     amount += unit * 4;
+//     break;
+// }
+
+console.log(amount);
