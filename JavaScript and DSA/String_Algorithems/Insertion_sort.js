c4f9d2d4ebd2f85cfc392a6b3831f0f27f58da49
Rajan-sharma-0 <nk800109@gmail.com>
2025-08-10 22:18:43 +0530

    insertion sorting

---
+++ JavaScript and DSA/String_Algorithems/Insertion_sort.js
@@ -0,0 +1,13 @@
+let arr = [10, 2, 12, 16, 6]
+let n = arr.length
+
+for(let i=1; i<n; i++){
+    let key = arr[i]
+    let j = i-1
+    while(j>=0 && arr[j]> key){
+        arr[j+1] = arr[j]
+        j--
+    }
+    arr[j+1] = key
+}
+console.log(arr);
